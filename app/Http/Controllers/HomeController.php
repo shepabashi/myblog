0771420a71b896a63acf0511e76a923744ff6a55
しぇぱっち <shepabashi@gmail.com>
2017-03-27 02:35:29 +0900

    Indexのページを作成し、Postのindexをルートに移動しました。

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
+     * Show the index page.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home');
+        $posts = Post::latest()->paginate();
+        return view('home')->withPosts($posts);
     }
 }
