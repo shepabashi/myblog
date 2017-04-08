6135a3f6132117ac9f32f0471f2ad50b86b11a54
しぇぱっち <shepabashi@gmail.com>
2017-04-08 14:45:37 +0900

    optionにaccessorを追加、getに関してはhelper関数に追加

--- app/Option.php
+++ app/Option.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
-    //
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'key', 'name',
+    ];
+
+    public static function get($key, $default = '')
+    {
+        $option = Option::where('key', $key)->first();
+        return $option ? $option->value : $default;
+    }
+
+    public static function set($key, $value)
+    {
+        $option = Option::firstOrCreate(['key' => $key]);
+        $option->value = $value;
+
+        if ($option->save()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
